4e790c5ebad2332b5a071e3dae6983709a77311a
podpirate <podpirate@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-13 20:07:48 +0000

    version 1.0.2, wc 2.1 issue fixed
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-pay-for-payment/trunk@857449 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- woocommerce-payforpayment.php
+++ woocommerce-payforpayment.php
@@ -3,7 +3,7 @@
 Plugin Name: WooCommerce Pay for Payment
 Plugin URI: http://wordpress.org/plugins/woocommerce-pay-for-payment
 Description: Setup individual charges for each payment method in woocommerce.
-Version: 1.0.1
+Version: 1.0.2
 Author: Jörn Lund
 Author URI: https://github.com/mcguffin
 
@@ -24,7 +24,8 @@ class Pay4Pay {
 
 	private function __construct() {
 		load_plugin_textdomain( 'pay4pay' , false, dirname( plugin_basename( __FILE__ )) . '/lang' );
-		add_filter( 'woocommerce_init' , array($this, 'add_payment_options') );
+		add_action( 'woocommerce_init' , array($this, 'add_payment_options') );
+		add_action( 'woocommerce_update_options_checkout' , array($this, 'add_payment_options') );
 		add_action( 'woocommerce_before_calculate_totals' , array($this,'add_pay4payment' ) );
 		add_action( 'woocommerce_review_order_after_submit' , array($this,'print_autoload_js') );
 	}
